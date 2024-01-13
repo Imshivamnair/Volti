9dd533b629ea6c752b314b18d1ff24bf5f106800
Kumar Shivam Nair <47927079+Imshivamnair@users.noreply.github.com>
2024-01-13 22:43:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,60 @@
  * #preloading
  */
 
-const loadElement = document.querySelector("[data-preloader");
+const loadElement = document.querySelector("[data-preloader]");
 
 window.addEventListener("load", function () {
     loadElement.classList.add("loaded");
 });
+
+
+/**
+ * #Mobile Navbar Toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const togglerNavbar = function () {
+    navbar.classList.toggle("active");
+    navToggler.classList.toggle("active");
+}
+
+navToggler.addEventListener("click", togglerNavbar);
+
+/**
+ *  #HEADER
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-go-top-btn]");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY >= 100) {
+        header.classList.add("active");
+        backTopBtn.classList.add("active");
+    } else {
+        header.classList.remove("active");
+        backTopBtn.classList.remove("active");
+    }
+});
+
+
+/**
+ * #SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+    for (let i = 0, x = revealElements.length; i < x; i++) {
+        if (revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.2) {
+            revealElements[i].classList.add("revealed");
+        } else {
+            revealElements[i].classList.remove("revealed");
+        }
+    }
+}
+
+window.addEventListener("scroll", scrollReveal);
+window.addEventListener("load", scrollReveal);
